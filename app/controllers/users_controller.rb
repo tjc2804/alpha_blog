dd38ab561da0c6e981f5475d88ef18a87c741c52
jichong <tay_ji_chong@hotmail.com>
2023-01-20 00:00:45 +0800

    update for section 6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
-    @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+  
+  def index
+   @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -10,14 +16,12 @@ class UsersController < ApplicationController
   end
 
   def edit
-   @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was sucessfully updated"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
@@ -27,6 +31,7 @@ class UsersController < ApplicationController
   def create
    @user = User.new(user_params)
    if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha Blog, #{@user.username} you have sucessfully sign up"
       redirect_to articles_path
    else
@@ -34,9 +39,27 @@ class UsersController < ApplicationController
    end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil if @user == current_user
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email,:password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user !=@user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own account"
+      redirect_to @user
+    end
   end
 
 end
